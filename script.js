da1e518f3ccfacb70516a86d2f2ce4969cbda526
Andrew Trigg <andrewt51@hotmail.com>
2016-04-03 14:32:43 +0100

    Vendor prefixes added

--- script.js
+++ script.js
@@ -14,21 +14,26 @@ let init = ()=>{
   ]
   let photoArrayMarker = 0;
   let rotation = 0
-
+  let clearVal=1;
+  let vid = document.getElementsByTagName('video')[0];
+  // vid.playbackRate = 1;
   let circle = document.getElementsByClassName('circle')[0];
   let translucency = document.getElementsByTagName('input')[0];
   translucency.addEventListener('change',activateClear)
   let cube = document.getElementById('cube');
+  cube.addEventListener('click', removeFace)
   let faces = document.getElementsByClassName('face');
   let order = 0;
-  let picFaces = [faces[1],faces[3],faces[0],faces[2]];
+  let picFaces = [faces[2],faces[4],faces[0],faces[3]];
   console.log(typeof faces)
  
   let buttons = document.getElementsByClassName('btn');
   buttons[0].addEventListener('click', leftBtn);
   buttons[1].addEventListener('click', rightBtn);
 
+  // for(let i=0, l=faces.length;i<l;i++){
 
+  // }
   function leftBtn(){
     // order = order > 2 ? 0 : order +1;
      order = order < 1 ? 3 : order -1;
@@ -44,7 +49,7 @@ let init = ()=>{
     // order = order < 1 ? 3 : order -1;
     order = order > 2 ? 0 : order +1;
     photoArrayMarker = photoArrayMarker <= 0 ? photoArray.length-1 :photoArrayMarker-1;
-    console.log(picFaces[1]);
+    // console.log(picFaces[1]);
     rotation = rotation + 90;
     cube.style.transform = "rotate3d(0,1,0,"+ rotation+"deg)"
     circle.style.transform = "translate3d(75px,90px,0) rotate3d(0,1,0,"+(-rotation)+"deg)"
@@ -52,12 +57,18 @@ let init = ()=>{
   }
   function activateClear(){
     
-    let clearVal =  document.getElementsByTagName("input")[0].value
+    clearVal =  document.getElementsByTagName("input")[0].value
     console.log(clearVal)
     for(let i = 0,l = faces.length; i<l;i++ ){
       faces[i].style.opacity = clearVal;
     }
 
+  }
+
+  function removeFace(event){
+    console.log(event.target.style.opacity)
+    event.target.style.opacity = event.target.style.opacity === '0' ? clearVal : '0';
+    // event.target.style.opacity = 0;
   }
 }
 
